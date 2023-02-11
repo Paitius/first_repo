a552c5b90d7e2e84a1bce919d468084f4bd80de6
Kamil Gębka <kgebka1996@gmail.com>
2023-02-11 11:55:14 +0100

    1

--- functions.py
+++ functions.py
@@ -1,9 +1,9 @@
-# def multiply(x,y):
-#     return round(x*y,5)
+def multiply(x, y):
+    return round(x*y, 5)
 
 
-# def no_of_letter(text):
-#     return len(text)
+def no_of_letter(text):
+    return len(text)
 
 
 def fizz_buzz(i):
--- tdd.py
+++ tdd.py
@@ -1,14 +1,16 @@
-from functions import fizz_buzz
+from functions import *
 
 
-# def multiply_test():
-#     assert multiply(5,10) == 50
-#     assert multiply(100, 1.1) == 110
-#     assert multiply(1.5,1.5) == 2.25
+def multiply_test():
+    assert multiply(5, 10) == 50
+    assert multiply(100, 1.1) == 110
+    assert multiply(1.5, 1.5) == 2.25
 
-# def no_of_letter_test():
-#     assert no_of_letter('kamil') == 5
-#     assert no_of_letter('') == 0
+
+def no_of_letter_test():
+    assert no_of_letter('kamil') == 5
+    assert no_of_letter('') == 0
+
 
 def fizz_buzz_test():
     assert fizz_buzz(1) == 1
