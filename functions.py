83703d986f92f30843ea9f3fe47fb7393b2263ab
Kamil Gębka <kgebka1996@gmail.com>
2023-02-11 11:49:35 +0100

    test2

--- functions.py
+++ functions.py
@@ -1,6 +1,17 @@
-def multiply(x,y):
-    return round(x*y,5)
+# def multiply(x,y):
+#     return round(x*y,5)
 
 
-def no_of_letter(text):
-    return len(text)
+# def no_of_letter(text):
+#     return len(text)
+
+
+def fizz_buzz(i):
+    if i % 3 == 0 and i % 5 == 0:
+        return "FizzBuzz"
+    elif i % 3 == 0:
+        return "Fizz"
+    elif i % 5 == 0:
+        return "Buzz"
+    else:
+        return i
--- tdd.py
+++ tdd.py
@@ -1,11 +1,15 @@
-from functions import *
+from functions import fizz_buzz
 
 
-def multiply_test():
-    assert multiply(5,10) == 50
-    assert multiply(100, 1.1) == 110
-    assert multiply(1.5,1.5) == 2.25
+# def multiply_test():
+#     assert multiply(5,10) == 50
+#     assert multiply(100, 1.1) == 110
+#     assert multiply(1.5,1.5) == 2.25
 
-def no_of_letter_test():
-    assert no_of_letter('kamil') == 5
-    assert no_of_letter('') == 0
+# def no_of_letter_test():
+#     assert no_of_letter('kamil') == 5
+#     assert no_of_letter('') == 0
+
+def fizz_buzz_test():
+    assert fizz_buzz(1) == 1
+    assert fizz_buzz(3) == 'Fizz'
